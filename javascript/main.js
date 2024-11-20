36a161297b58fd201a6310a3671a106a4cee009c
ashlaw05 <ashley.lawless@edu.sait.ca>
2024-11-20 10:27:18 -0700

    button
    an edit was made to the button in which the text changes when the user hovers over it

--- javascript/main.js
+++ javascript/main.js
@@ -5,6 +5,31 @@ const d = new Date();
 const yearElement = document.getElementById("footerYear").textContent = currentYear;
 console.log(d);
 
+// greeting
+
+window.onload = function() {
+    const currentTime = new Date().getHours(); 
+    const greetingElement = document.getElementById("greeting");
+
+    let greetingMessage = "";
+    let greetingStyle = "";
+
+    if (currentTime >= 5 && currentTime < 12) {
+        greetingMessage = "Morning sunshine!";
+        greetingStyle = "morning"; 
+    } else if (currentTime >= 12 && currentTime < 18) {
+        greetingMessage = "Afternoon my friend!";
+        greetingStyle = "afternoon"; 
+    } else {
+        greetingMessage = "Evening!";
+        greetingStyle = "evening"; 
+    }
+console.log(greetingMessage);
+
+    greetingElement.textContent = greetingMessage;
+    greetingElement.className = greetingStyle;
+};
+
 
 // date/button code
 
@@ -17,6 +42,15 @@ function openpopup(){
 function closepopup(){
     popup.classList.remove("open-popup");
 }
+
+const button = document.querySelector('button');
+button.addEventListener('mouseenter', function ()  {
+    button.textContent = "Please I Beg!";
+});
+
+button.addEventListener('mouseleave', function () {
+    button.textContent = "Click Me!"
+});
 
 // loop code
 
